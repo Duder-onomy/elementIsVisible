83a1283c1fa090abc36cde54e04abe6399425179
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2015-07-09 18:22:47 -0700

    Removing underscores from the module name....unsure why they were there to begin with . Habits. or maybe Hobbits. Bumping version 0.0.2

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) : global._elementIsVisibile = factory();
+    typeof define === 'function' && define.amd ? define(factory) : global.elementIsVisibile = factory();
 }(this, function () {
     'use strict';
 
-    function _elementIsVisibile(element) {
+    function elementIsVisibile(element) {
         if (element.nodeType !== 1 || element.isEqualNode(document.body)) {
             return true;
         }
 
         if (element.currentStyle && element.currentStyle.display !== 'none' && element.currentStyle.visibility !== 'hidden') {
-            return _elementIsVisibile(element.parentNode);
+            return elementIsVisibile(element.parentNode);
         } else if (window.getComputedStyle) {
             var computedStyle = document.defaultView.getComputedStyle(element, null);
             if (computedStyle.getPropertyValue('display') !== 'none' && computedStyle.getPropertyValue('visibility') !== 'hidden') {
-                return _elementIsVisibile(element.parentNode);
+                return elementIsVisibile(element.parentNode);
             }
         }
         return false;
     }
 
-    return _elementIsVisibile;
+    return elementIsVisibile;
 }));
